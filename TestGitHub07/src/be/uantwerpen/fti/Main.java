7b3a80f22cf28e7c80ec501381cfe0844e4dd113
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-05-15 09:51:08 +0200

    start project, no ignore file

---
+++ TestGitHub07/src/be/uantwerpen/fti/Main.java
@@ -0,0 +1,9 @@
+package be.uantwerpen.fti;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello............");
+    }
+}
