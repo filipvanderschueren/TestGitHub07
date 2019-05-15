eace0f1a6238d31165253eea46a9421f15d827e8
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-05-15 09:59:52 +0200

    add new Class Message

--- TestGitHub07/src/be/uantwerpen/fti/Main.java
+++ TestGitHub07/src/be/uantwerpen/fti/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello............");
+
+        Message m = new Message();
+        m.setMessage( "a new Message from Filip" );
+        System.out.println("Message............" + m.getMessage());
+
     }
 }
